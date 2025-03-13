25212f01b964eaa457905abcd01ad8e21fbf478e
muralicse422 <111060152+muralicse422@users.noreply.github.com>
2025-03-13 21:37:39 +0530

    Added UserDetails ecomerce skeliton application

---
+++ UserDetailsMicro/src/main/java/com/ecom/userDetails/UserDetailsMicro/UserDetailsMicroApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.userDetails.UserDetailsMicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDetailsMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDetailsMicroApplication.class, args);
+	}
+
+}
